3a482f08d662f2afaa5281e5eaa9d8710230742b
Jonathan Lomas <jonathan@floatinglomas.ca>
2016-09-08 17:23:31 -0700

    Add some readme-ish stuff to config/template.js

--- config/template.js
+++ config/template.js
@@ -1,5 +1,9 @@
 'use strict';
 
+// Save this file as `index.js` in this folder with your account's IDs.
+// `index.js` is .gitignored so it won't accidentally end up somewhere
+// it shouldn't.
+
 module.exports = exports = {
   clientId: 'ca_YOUR_CLIENT_ID', // Your client ID: https://dashboard.stripe.com/account/applications/settings
   secretKey: 'sk_YOUR_SECRET_KEY', // Your secret API KEY: https://dashboard.stripe.com/account/apikeys
