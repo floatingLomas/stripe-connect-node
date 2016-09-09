8b044041a0fba382fde5b5bd32f1ee5feaa3cea4
Jonathan Lomas <jonathan@floatinglomas.ca>
2016-09-09 13:34:34 -0700

    Use localhost:3000 for template

--- config/template.js
+++ config/template.js
@@ -7,7 +7,7 @@
 module.exports = exports = {
   clientId: 'ca_YOUR_CLIENT_ID', // Your client ID: https://dashboard.stripe.com/account/applications/settings
   secretKey: 'sk_YOUR_SECRET_KEY', // Your secret API KEY: https://dashboard.stripe.com/account/apikeys
-  redirectUri: 'http://localhost/connected', // https://dashboard.stripe.com/account/applications/settings
+  redirectUri: 'http://localhost:3000/connected', // https://dashboard.stripe.com/account/applications/settings
 
   port: 3000
 };
